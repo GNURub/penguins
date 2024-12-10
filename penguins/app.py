682250c7b2b03413e2b27c2f7a6182aaae9bb756
Ruben <rubencidlara@gmail.com>
2024-12-10 14:44:18 +0100

    Actualizar función de predicción para incluir variables categóricas y agregar consultas en Jupyter Notebook

--- penguins/app.py
+++ penguins/app.py
@@ -1,24 +1,27 @@
 import pickle
+import numpy as np
 from flask import Flask, jsonify, request
 
 classes = ['Adelie', 'Chinstrap', 'Gentoo']
 
 def predict_single(penguin, vectorizer, scaler, model):
     # No se como hacer participe a las variables categoricas para la prediccion
-    penguin_categoricas = vectorizer.transform([{
+    penguin_cat = vectorizer.transform([{
          'island': penguin['island'],
          'sex': penguin['sex']
      }])
     
-    penguin_sdt = scaler.transform([[
+    penguin_num = scaler.transform([[
         penguin['bill_length_mm'],
         penguin['bill_depth_mm'],
         penguin['flipper_length_mm'],
         penguin['body_mass_g']
     ]])
+
+    penguin_val = np.hstack((penguin_cat, penguin_num))
     
-    y_pred = model.predict(penguin_sdt)[0]
-    y_prob = model.predict_proba(penguin_sdt)[0][y_pred]
+    y_pred = model.predict(penguin_val)[0]
+    y_prob = model.predict_proba(penguin_val)[0][y_pred]
     return (y_pred, y_prob)
 
 def predict(vectorizer, scaler, model):
